7c83e48b00b6d41128a84303b61e5b6e26086e4a
Jenny Chau <jnechau@gmail.com>
2021-12-13 23:43:45 -0500

    commit tree d619f5012b4bea93e0bb8833c76836f4d031bfe9

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
+    # split at a period or question mark or exclamation mark and filter/add any to the array that aren't empty. 
   end
 end
+
+pp "frightful.".sentence?
